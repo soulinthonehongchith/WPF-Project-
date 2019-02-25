634ff08ac0634cba6fffbaa73b19fa84795ff86f
Sou Hongchith <soulinthonehongchith@ou.edu>
2019-02-25 11:48:48 -0600

    No picture, no quote, no character.

---
+++ WPF-JSON/JSON/HerokuQuote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    class HerokuQuote
+    {
+        public string character { get; set; }
+        public string quote { get; set; }
+
+    }
+}
